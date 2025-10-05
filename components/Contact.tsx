317e1c2f2a8a10d3d8d9076b48de0b4d6c86be00
ptang0114 <pt2444@columbia.edu>
2025-10-05 23:00:05 +0800

    修复Contact组件中的文本转义问题，将单引号替换为HTML实体&apos;以确保正确显示。

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -68,7 +68,7 @@ const Contact: React.FC<ContactProps> = ({ isDarkMode }) => {
         transition={{ delay: 0.7, duration: 0.5 }}
         className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'
       >
-        I'd love to hear from you! If you have any questions, comments, or feedback, please use the form below.
+        I&apos;d love to hear from you! If you have any questions, comments, or feedback, please use the form below.
       </motion.p>
 
       <motion.form
